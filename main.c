75608e30e26f8ad8a9f7b9ac2e6b874715c63b8c
ccpink <ccpink911@gmail.com>
2025-11-18 14:22:15 -0400

    Psuedo Code of program as imagined

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    //Initialize Variables
+
+    //CREATE PIPES
+
+    //ASK FOR USER INPUT
+
+    //FORK
+
+    //While Loop
+
+    //IF child
+
+        //Read Pipe
+
+        //Flip characters
+
+        //Write to secondary pipe
+
+    //IF Error
+
+        //Print Error
+
+    //IF Parent
+
+        //Wait for response
+
+        //Get New User Input
+
+        //Pipe Input
+
     return 0;
 }
