a9a6592398487985e5fbfce8179e767a754b6c21
ccpink <ccpink911@gmail.com>
2025-11-20 20:21:18 -0400

    Program mostly completed time to error handle.

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,56 +8,55 @@
 
 int main(void) {
     //Initialize Variables
+    int wait_status;
     int max_input = 254;
-    int length;
     pid_t pid;
-    int can_run = 1;
-    //Initialize
-    int parent_to_child[2];
-    int child_to_parent[2];
-
-    //Create the pipes
-    if (pipe(parent_to_child) == -1) {
-        perror("pipe_parent_to_child");
-        exit(EXIT_FAILURE);
-    }
-    if (pipe(child_to_parent) == -1) {
-        perror("pipe_child_to_parent");
-        exit(EXIT_FAILURE);
-    }
-
-    //FORK
-    pid = fork();
-
-    if (pid == -1) {
-        perror("Problem creating child");
-        return 0;
-    }
-
-    //Close pipes the parent/child will not ever user before the loop
-    if (pid == 0)
-    {
-        close(child_to_parent[0]);
-        close(parent_to_child[1]);
-    }
-    else
-    {
-        close(child_to_parent[1]);
-        close(parent_to_child[0]);
-    }
+    int i = 0;
+    char single_char;
 
 
+    while (1){
+        //FORK
+        int parent_to_child[2];
+        int child_to_parent[2];
 
-    if (pid == 0)   //IF Child
-    {
-        while (1){
+        //Create the pipes
+        if (pipe(parent_to_child) == -1) {
+            perror("pipe_parent_to_child");
+            exit(-1);
+        }
+        if (pipe(child_to_parent) == -1) {
+            perror("pipe_child_to_parent");
+            close(parent_to_child[0]);
+            close(parent_to_child[1]);
+            exit(-1);
+        }
+
+        pid = fork();
+
+        if (pid == -1) {
+            perror("Problem creating child");
+            close(parent_to_child[0]);
+            close(parent_to_child[1]);
+            close(child_to_parent[0]);
+            close(child_to_parent[1]);
+            exit(-1);
+        }
+
+        if (pid == 0)
+        {
+            close(child_to_parent[0]);
+            close(parent_to_child[1]);
+
             char child_buffer[255];
+            i = 0;
             //Wait on parent writing
-            while (read(parent_to_child[0], &child_buffer, 1) > 0)
+            while (read(parent_to_child[0], &single_char, 1) > 0)
             {
-
+                child_buffer[i] = single_char;
+                i++;
             }
-            //Read Pipe
+            child_buffer[i] = '\0';
 
             //If output is close exit
             if (strcmp(child_buffer, "close") == 0) {
@@ -65,39 +65,61 @@ int main(void) {
                 exit(1);
             }
 
-            //Flip characters
+            for (int j = 0; j < i; ++j) {
+                if (isupper(child_buffer[j]))
+                {
+                    child_buffer[j] = (char)tolower(child_buffer[j]);
+                }
+                else if (islower(child_buffer[j]))
+                {
+                    child_buffer[j] = (char)toupper(child_buffer[j]);
+                }
+            }
+
+            printf("The String from Child to Parent is: %s", child_buffer);
 
             //Write to secondary pipe
             write(child_to_parent[1], child_buffer, strlen(child_buffer) + 1);
+
+            close(parent_to_child[0]);
+            close(child_to_parent[1]);
+            exit(1);
         }
-    }
-    else    //IF Parent
-    {
-        while (1){
+        else
+        {
+            close(child_to_parent[1]);
+            close(parent_to_child[0]);
+
             char parent_buffer[255];
 
+            printf("Please Enter Your Sentence: ");
             //Get New User Input
             fgets(parent_buffer, max_input, stdin);
+            parent_buffer[strcspn(parent_buffer, "\n")] = '\0';
 
             //Send to Child.
             write(parent_to_child[1], parent_buffer, strlen(parent_buffer) + 1);
-
+            close(parent_to_child[1]);
             //If input was close exit.
             if (strcmp(parent_buffer, "close") == 0) {
                 close(child_to_parent[0]);
-                close(parent_to_child[1]);
                 exit(1);
             }
 
-            while (read(child_to_parent[0], &parent_buffer, 1) > 0)
+            wait(&wait_status);
+
+            i = 0;
+            while (read(child_to_parent[0], &single_char, 1) > 0)
             {
-
+                parent_buffer[i] = single_char;
+                i++;
             }
-            //Ouput Child Response
+            parent_buffer[i] = '\0';
 
+            //Output Child Response
+            printf("The String Received by Parent is: %s", parent_buffer);
 
         }
     }
-
     return 0;
 }
